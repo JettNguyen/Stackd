49acfcd4da89e56c3ac315b4a3af12d8b7bb08a4
ufmeiqili <127431433+ufmeiqili@users.noreply.github.com>
2026-03-03 00:32:13 -0500

    Revert "finished login & signup pages with styling."
    This reverts commit 45fd63943e5c97cf847af024fcc4fa353b429365.

--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -1,51 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import './LoginSignup.css';
-import logo from '../assets/logo.png';
 
 const LoginSignup = () => {
-  const navigate = useNavigate();
-  const [isLogin, setIsLogin] = useState(true);
-
   return (
     <div className="login-signup-page">
-      <div class="login-signup-header">
-        <div className="logo">
-          <img src={logo} alt="Stackd Logo" className="login-signup-logo-image" />
-          <h1 className="login-signup-logo-text">Stackd</h1>
-        </div>
-      </div>
-      {isLogin ? (
-        <form className="login-signup-form">
-          <div className="login-signup-form-comp">
-            <p className="login-signup-form-title">Username</p>
-            <input className="login-signup-form-input" type="text"></input>
-          </div>
-          <div className="login-signup-form-comp">
-            <p className="login-signup-form-title">Password</p>
-            <input className="login-signup-form-input" type="text"></input>
-          </div>
-          <button className="login-button" type="button" onClick={() => navigate('/')}>Log in</button>
-          <button className="switch-button" type="button" onClick={() => setIsLogin(false)}>Don't have one?<br/>Create one!</button>
-        </form>
-      ) : (
-        <form className="login-signup-form">
-          <div className="login-signup-form-comp">
-            <p className="login-signup-form-title">Email</p>
-            <input className="login-signup-form-input" type="text"></input>
-          </div>
-          <div className="login-signup-form-comp">
-            <p className="login-signup-form-title">Username</p>
-            <input className="login-signup-form-input" type="text"></input>
-          </div>
-          <div className="login-signup-form-comp">
-            <p className="login-signup-form-title">Password</p>
-            <input className="login-signup-form-input" type="text"></input>
-          </div>
-          <button className="login-button" type="button" onClick={() => navigate('/')}>Create Account</button>
-          <button className="switch-button" type="button" onClick={() => setIsLogin(true)}>Already have one?<br/>Log in!</button>
-        </form>
-      )}
+      <h1>Login / Sign Up</h1>
+      <p>todo</p>
     </div>
   );
 };
